e35bca857893df4f3198c420829d136bbe47f1d3
Florian Fesseler <ffesseler@kapit.fr>
2014-04-18 18:28:37 +0200

    Added login example

--- client/test/e2e/simple.js
+++ client/test/e2e/simple.js
@@ -1,10 +1,19 @@
-describe('simple test', function () {
+describe('login', function () {
 
     beforeEach(function() {
         browser.get('/');
     });
 
-    it('should do something', function () {
-        expect(true).toBe(true);
+    it('should login as toto', function () {
+        var loginBtn = element(by.css('.login'));
+        expect(loginBtn.isPresent()).toBe(true);
+        loginBtn.click();
+        var loginModal = element(by.css('.modal'));
+        expect(loginModal.isPresent()).toBe(true);
+        element(by.model('user.email')).sendKeys('toto@gmail.com');
+        element(by.model('user.password')).sendKeys('password');
+        element(by.buttonText('Sign in')).click().then(function () {
+            expect(loginModal.isPresent()).toBe(false);
+        });
     });
 });
