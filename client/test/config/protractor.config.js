f0bf8132cf1b5c47be6610ec57e5a101153cf107
Florian Fesseler <ffesseler@kapit.fr>
2014-04-21 12:19:46 +0200

    Moved login in config prepare function

--- client/test/config/protractor.config.js
+++ client/test/config/protractor.config.js
@@ -12,6 +12,25 @@ exports.config = {
     }
   },
 
+  onPrepare: function() {
+
+    browser.params.globalDataForTests = true;
+
+    browser.driver.get('http://localhost:3000/');
+
+    browser.driver.findElement(by.css('.login')).click();
+    browser.driver.findElement(by.name('login')).sendKeys('toto@gmail.com');
+    browser.driver.findElement(by.name('pass')).sendKeys('password');
+    browser.driver.findElement(by.css('.btn-primary')).click();
+
+    browser.driver.wait(function() {
+      return browser.driver.isElementPresent(by.css('.modal')).then(function(visible) {
+        return visible === false;
+      })
+    });
+
+  },
+
   baseUrl: 'http://localhost:3000'
 
 };
--- client/test/e2e/simple.js
+++ client/test/e2e/simple.js
@@ -1,19 +1,10 @@
 describe('login', function () {
 
     beforeEach(function() {
-        browser.get('/');
+        browser.get('/admin/projects');
     });
 
     it('should login as toto', function () {
-        var loginBtn = element(by.css('.login'));
-        expect(loginBtn.isPresent()).toBe(true);
-        loginBtn.click();
-        var loginModal = element(by.css('.modal'));
-        expect(loginModal.isPresent()).toBe(true);
-        element(by.model('user.email')).sendKeys('toto@gmail.com');
-        element(by.model('user.password')).sendKeys('password');
-        element(by.buttonText('Sign in')).click().then(function () {
-            expect(loginModal.isPresent()).toBe(false);
-        });
+      expect(browser.params.globalDataForTests).toBe(true);
     });
 });
